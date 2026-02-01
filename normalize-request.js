ceac1ebccdf172dd1d3387090ed27a1895b13831
Devansh <103169058+Deva1234567@users.noreply.github.com>
2026-02-01 19:02:29 +0530

    Add normalize-request.js with initial configuration

---
+++ normalize-request.js
@@ -0,0 +1,39 @@
+{
+  "nodes": [
+    {
+      "parameters": {
+        "assignments": {
+          "assignments": [
+            {
+              "id": "da9a5d41-ef45-41f9-865d-6e0a84077e57",
+              "name": "=debug_received: {{ JSON.stringify($json) }}",
+              "value": "",
+              "type": "string"
+            }
+          ]
+        },
+        "options": {}
+      },
+      "type": "n8n-nodes-base.set",
+      "typeVersion": 3.4,
+      "position": [
+        -496,
+        48
+      ],
+      "id": "75de5d6d-636f-47e6-a5d5-c4a72f6a5093",
+      "name": "Edit Fields"
+    }
+  ],
+  "connections": {
+    "Edit Fields": {
+      "main": [
+        []
+      ]
+    }
+  },
+  "pinData": {},
+  "meta": {
+    "templateCredsSetupCompleted": true,
+    "instanceId": "72e110e0bce684e9c6755951dc8070aa1494b0744a82e8f4223c0bdfc231fa22"
+  }
+}
