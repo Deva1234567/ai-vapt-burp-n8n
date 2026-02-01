3e1a9a4f2a5afd4b841629e19f630d856df6d2fe
Devansh <103169058+Deva1234567@users.noreply.github.com>
2026-02-01 19:01:35 +0530

    Add parse-burp-xml.js for XML parsing configuration

---
+++ parse-burp-xml.js
@@ -0,0 +1,34 @@
+{
+  "nodes": [
+    {
+      "parameters": {
+        "dataPropertyName": "xml_text",
+        "options": {
+          "attrkey": "@",
+          "mergeAttrs": true,
+          "normalize": false
+        }
+      },
+      "type": "n8n-nodes-base.xml",
+      "typeVersion": 1,
+      "position": [
+        -368,
+        -192
+      ],
+      "id": "95af1f8a-c38b-43a5-9b43-382931475218",
+      "name": "XML"
+    }
+  ],
+  "connections": {
+    "XML": {
+      "main": [
+        []
+      ]
+    }
+  },
+  "pinData": {},
+  "meta": {
+    "templateCredsSetupCompleted": true,
+    "instanceId": "72e110e0bce684e9c6755951dc8070aa1494b0744a82e8f4223c0bdfc231fa22"
+  }
+}
